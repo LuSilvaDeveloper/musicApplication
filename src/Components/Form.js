9681f51624df1bfc552ac653cf4ac870308fb8d9
Jina <77096179+Jina-sw@users.noreply.github.com>
2022-10-10 03:04:21 -0700

    10/10

---
+++ src/Components/Form.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+/*
+Jina
+*/
+function Form(props) {
+    
+    const [music, setMusic] = useState("");
+
+    function handleChange(e){
+        setMusic(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        props.addSongs(music);
+        setMusic("");
+    }
+
+
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <h2>
+                <label htmlFor="new-music-title" className="encouragement">Create my one and only Playlist today</label>
+            </h2>
+            <div className="align">
+                <input
+                    type="text"
+                    id="newMusic"
+                    className="enterMusic"
+                    name="text"
+                    autoComplete="off"
+                    value={music}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>{"\n"}</div>
+            <div className="align gap">
+                <button type="submit" className="add button">
+                    Add on my playlist
+                </button>
+            </div>
+            <div>{"\n"}</div>
+            <div className="align gap">
+                <button type="button" className="clear button">
+                    Clear all music
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default Form; 
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,3 +1,4 @@
+
 function Menu(props){
 
     return (
